3aa85997c59df989ddeda328b0bf55b6b2c822b8
snehal patil <patilonlysnehal2003@gmail.com>
2024-08-20 13:01:08 +0530

    Audio Page updated

--- calmify-backend/backend/urls.py
+++ calmify-backend/backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from api.views import upload_video, home
+from . import speech_to_text
 
 urlpatterns = [
     path('', home, name='home'),
     path('upload/', upload_video, name='upload_video'),
     path('admin/', admin.site.urls),
+    path('api/speech-to-text/', speech_to_text.convert_speech_to_text, name='speech_to_text'),
 ]
