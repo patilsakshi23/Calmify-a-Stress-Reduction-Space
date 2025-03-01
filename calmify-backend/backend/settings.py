e3fff6ec1b0028fc935abb13119978c784808409
patilsakshi23 <sakshipatil0803@gmail.com>
2025-03-01 19:39:30 +0530

    chnaged url

--- calmify-backend/backend/settings.py
+++ calmify-backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-8)c!-n^t80n%2n0$=hllr)4cscqf*)lnrtvri)kj6inu1x674=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1:8000","https://calmify-a-stress-reduction-space.onrender.com/"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1:8000","https://calmify-a-stress-reduction-space.onrender.com"]
 
 
 
